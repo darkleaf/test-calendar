2bd7b8e0bbce4968d3b0022cda046f0785d24919
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2016-01-13 17:31:44 +0300

    show not_authorized flash

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -1,3 +1,12 @@
 class Web::ApplicationController < ApplicationController
   include UserAuthentication
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = t(:not_authorized)
+    redirect_to(request.referrer || root_path)
+  end
 end
