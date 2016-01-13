191283b89d110b9ac769935acb44c3ff63579a2f
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2016-01-13 15:11:56 +0300

    map sql result to correct hash

--- app/queries/event_objects_filter_query.rb
+++ app/queries/event_objects_filter_query.rb
@@ -5,10 +5,9 @@ module EventObjectsFilterQuery
     return [] if [start_date, end_date].any? &:blank?
 
     sql = ActiveRecord::Base.send(:sanitize_sql_array, [<<-SQL, start_date, end_date])
-      SELECT days.day AS start,
+      SELECT days.day,
              events.id,
-             events.name AS title,
-             ('/events/' || events.id || '/edit') AS url
+             events.name
       FROM (
         SELECT seria AS day
         FROM generate_series(?::timestamp, ?::timestamp, '1 day') AS seria
@@ -21,7 +20,17 @@ module EventObjectsFilterQuery
       ) as events ON true
     SQL
 
+    result = ActiveRecord::Base.connection_pool.with_connection { |con| con.exec_query(sql) }
 
-    res = ActiveRecord::Base.connection_pool.with_connection { |con| con.exec_query(sql) }
+    result.map(&method(:row_to_event_object))
+  end
+
+  def row_to_event_object(row)
+    {
+        id: row['id'],
+        title: row['name'],
+        start: row['day'].to_date,
+        url: Rails.application.routes.url_helpers.edit_event_path(row['id'])
+    }
   end
 end
